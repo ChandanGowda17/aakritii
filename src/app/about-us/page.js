f01a9dc136a436370fbbd07e6e89848c341e8d4e
ChandanGowda17 <chandangowda4054@gmail.com>
2026-07-09 22:47:21 +0530

    new pages

---
+++ src/app/about-us/page.js
@@ -0,0 +1,9 @@
+import CommunitySection from "../components/CommunitySection";
+
+export default function AboutUsPage() {
+  return (
+    <main className="bg-[#F8F1E6] pt-[86px] max-lg:pt-[72px]">
+      <CommunitySection />
+    </main>
+  );
+}
